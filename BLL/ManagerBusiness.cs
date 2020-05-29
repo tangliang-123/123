0d3bdba201de08e348189b9f0c8c6b9684688bc4
tangliang-123 <2596142718@qq.com>
2020-05-29 11:31:52 +0800

    0529

--- BLL/ManagerBusiness.cs
+++ BLL/ManagerBusiness.cs
@@ -24,7 +24,17 @@ namespace BLL
         {
             return DAL.ManagerDAL.ManagerLogin(admin, password);
         }
-
+        /// <summary>
+        /// 商家登录验证
+        /// </summary>
+        /// <param name="account"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static bool Permanlogin(string admin, string password)
+        {
+            return DAL.PermissionDAL.Permanlogin(admin, password);
+        }
+        //注册
         public static bool AddManager(Manager manager)
         {
             //判断账号是否存在？
@@ -87,6 +97,15 @@ namespace BLL
             return DAL.ManagerDAL.selectzh(manager);
         }
         /// <summary>
+        /// 查询所有商家信息
+        /// </summary>
+        /// <param name="jobDetail"></param>
+        /// <returns></returns>
+        public static DataSet selectmanager(Manager manager)
+        {
+            return DAL.ManagerDAL.selectmanager(manager);
+        }
+        /// <summary>
         /// 查询发布职位
         /// </summary>
         /// <param name="jobDetail"></param>
@@ -106,6 +125,15 @@ namespace BLL
             return DAL.JobDetailDAL.DropJob(jobDetail);
         }
         /// <summary>
+        /// 根据工作编号来获取工作对象
+        /// </summary>
+        /// <param name="jobDetail"></param>
+        /// <returns></returns>
+        public static JobDetail GetJob(string OrderNumber)
+        {
+            return DAL.JobDetailDAL.GetJob(OrderNumber);
+        }
+        /// <summary>
         /// 修改岗位状态
         /// </summary>
         /// <param name="jobDetail"></param>
@@ -123,6 +151,29 @@ namespace BLL
         public static bool updatejobxx(JobDetail jobDetail)
         {
             return DAL.JobDetailDAL.updatejobxx(jobDetail);
+        }
+        /// <summary>
+        /// 获取申请应聘简历信息
+        /// </summary>
+        /// <param name="jobdel"></param>
+        /// <returns></returns>
+        public static List<Job_appliction_record> GetAppliction_Records()
+        {
+            return DAL.Job_appliction_recordDAL.GetAppliction_Records();
+        }
+        /// <summary>
+        /// 申请应聘工作岗位状态操作
+        /// </summary>
+        /// <param name="jobdel"></param>
+        /// <returns></returns>
+        public static bool ChangeAppliction_recordState(int Jobid, byte state)
+        {
+            return DAL.Job_appliction_recordDAL.ChangeAppliction_recordState(Jobid, state);
+        }
+
+        public static JobDetail GetJobAppliction(string obAppliction_recordState)
+        {
+            return DAL.JobDetailDAL.GetJobAppliction(obAppliction_recordState);
         }
     }
 }
--- WebSite1/Default.aspx.cs
+++ WebSite1/Default.aspx.cs
@@ -20,7 +20,7 @@ public partial class _Default : System.Web.UI.Page
         
         if (admin != string.Empty && password != string.Empty)
         {
-            if (BLL.ManagerBusiness.ManagerLogin(admin, password)&&BLL.ManagerBusiness.Permanlogin(admin, password))
+            if (BLL.ManagerBusiness.ManagerLogin(admin, password)&&BLL.ManagerBusiness.Permanlogin(admin,password))
             {
                 ////登录成功，数据存入session中
                 Session["admin"] = admin;
@@ -30,6 +30,11 @@ public partial class _Default : System.Web.UI.Page
             if (BLL.TeachAdminBLL.T_StuAdminLogin(admin, password) && BLL.TeachAdminBLL.Peradmlogin(admin, password))
             {
                 Session["adminlogin"] = admin;
+                Utility.JavaScript.AlertAndRedirect("登录成功，点击确定跳转首页", "../shouye.aspx", this);
+            }
+            if (BLL.UniversityStudentBLL.StuLogin(admin, password) && BLL.UniversityStudentBLL.Perstulogin(admin, password))
+            {
+                Session["stulogin"] = admin;
                 Utility.JavaScript.AlertAndRedirect("登录成功，点击确定跳转首页", "../shouye.aspx", this);
             }
             else
