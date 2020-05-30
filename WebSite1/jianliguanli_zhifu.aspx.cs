0c0d8c675b861131dc20ae62ced5c5325d3a0dc3
xbekl <1741686466@qq.com>
2020-05-30 14:41:06 +0800

    55

---
+++ WebSite1/jianliguanli_zhifu.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class jianliguanli_zhifu : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Label10_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("jianliguanli.aspx");
+    }
+
+    protected void Label11_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("jianliguanlishixi.aspx");
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("fabu_tuijiangangwei.aspx");
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("fabu_tuijiangangwei.aspx");
+    }
+}
